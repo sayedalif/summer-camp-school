da139d85a6b72a66181b4b0d3e252defe83e9620
sayedalif <sayedalif18@gmail.com>
2024-01-14 19:05:50 +0600

    functional footer created

--- summerCampSchoolClient/src/pages/Home/Home.jsx
+++ summerCampSchoolClient/src/pages/Home/Home.jsx
@@ -1,11 +1,15 @@
 import { Outlet } from 'react-router-dom';
 import Navbar from '../shared/Navbar';
+import Footer from '../shared/Footer';
 
 const Home = () => {
   return (
     <div>
       <Navbar></Navbar>
+      <div className='min-h-[calc(100vh-276px)]'>
       <Outlet></Outlet>
+      </div>
+      <Footer></Footer>
     </div>
   );
 };
--- summerCampSchoolClient/src/pages/shared/Navbar.jsx
+++ summerCampSchoolClient/src/pages/shared/Navbar.jsx
@@ -3,7 +3,7 @@ import useAuth from "../../hooks/useAuth";
 
 const Navbar = () => {
   const location = useLocation();
-  console.log("🚀 ~ Navbar ~ location:", location);
+  // console.log("🚀 ~ Navbar ~ location:", location);
   const { user, signOut } = useAuth();
 
   return (
@@ -14,25 +14,20 @@ const Navbar = () => {
             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
           </div>
           <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-            <li><a>Item 1</a></li>
-            <li>
-              <a>Parent</a>
-              <ul className="p-2">
-                <li><a>Submenu 1</a></li>
-                <li><a>Submenu 2</a></li>
-              </ul>
-            </li>
-            <li><a>Item 3</a></li>
+          <Link className="font-semibold" to={`/`}>Home</Link>
+          <Link className="font-medium" to={`/instructors`}>Instructors</Link>
+          <Link to={`/classes`}>Classes</Link>
+          {user && <Link to={`/dashboard`}>Dashboard</Link>}
           </ul>
         </div>
-        <Link to={`/`} className="btn btn-ghost text-xl">ShutterCraft</Link>
+        <Link to={`/`} className="btn btn-ghost text-xl text-[#323232]">ShutterCraft</Link>
       </div>
       <div className="navbar-center hidden lg:flex">
         <ul className="menu menu-horizontal px-1 flex space-x-16">
           <Link className="font-semibold" to={`/`}>Home</Link>
           <Link className="font-medium" to={`/instructors`}>Instructors</Link>
           <Link to={`/classes`}>Classes</Link>
-          <Link to={`/dashboard`}>Dashboard</Link>
+          {user && <Link to={`/dashboard`}>Dashboard</Link>}
         </ul>
       </div>
       <div className="navbar-end">
@@ -50,6 +45,7 @@ const Navbar = () => {
             :
             !user && <Link to={`/login`} className="bg-[#3B0200] text-white rounded-3xl px-6 py-2">Sign in</Link>
         }
+
         {
           user &&
           <div className="dropdown dropdown-end">
