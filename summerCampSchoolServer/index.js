b2825d05e0a54c1cbb7b3111380433e8db511dfd
sayedalif <sayedalif18@gmail.com>
2024-01-27 08:45:11 +0600

    server connected to mongoDb

--- summerCampSchoolServer/index.js
+++ summerCampSchoolServer/index.js
@@ -12,6 +12,49 @@ app.get('/', (req, res) => {
 });
 
 
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.iegqqxy.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    // save user to database
+    app.put('/users/:email', async (req, res) => {
+      const email = req.params.email;
+      const user = req.body;
+      // console.log("🚀 ~ file: index.js:39 ~ app.put ~ user:", user);
+      const filter = { email: email };
+      const option = { upsert: true }
+      const updateDoc = {
+        $set: user
+      };
+
+      const result = await usersCollection.updateOne(filter, updateDoc, option);
+      // console.log(result);
+      res.send(result);
+    });
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
 
 app.listen(port, () => {
   console.log(`summer camp school app listening on port ${port}`)
