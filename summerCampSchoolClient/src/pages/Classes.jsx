e699a3b565e26cc4951d79c1d769c94d8e7fce5d
sayedalif <sayedalif18@gmail.com>
2024-05-09 01:48:30 +0600

    classes route and all data are added to the route

--- summerCampSchoolClient/src/pages/Classes.jsx
+++ summerCampSchoolClient/src/pages/Classes.jsx
@@ -1,9 +1,24 @@
+import ClassesCards from "../components/ClassesCards";
+import useFetch from "../hooks/utils/utils";
 
 
 const Classes = () => {
-  return (
-    <div>
+  const { data: classes = [], error, loading } = useFetch('/classes');
+  if (loading) {
+    return <h1>Loading...</h1>
+  }
+  if (error) {
+    return <h1>{error}</h1>
+  }
 
+  return (
+    <div className='lg:flex lg:flex-row lg:justify-between lg:flex-wrap md:flex md:flex-row md:justify-between md:flex-wrap sm:flex sm:flex-wrap lg:my-8 lg:mx-4 mt-4 mb-4 flex flex-col items-center'>
+      {
+        classes?.length > 0 && Array?.isArray(classes) &&
+        classes?.map((eachClass) => {
+          return <ClassesCards key={eachClass._id} eachClass={eachClass}></ClassesCards>
+        })
+      }
     </div>
   );
 };
