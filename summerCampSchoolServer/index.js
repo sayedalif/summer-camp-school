50d2c12208ecc545daaba99039a60405944c1a17
sayedalif <sayedalif18@gmail.com>
2024-05-28 00:24:56 +0600

    table added to enrolled classes

--- summerCampSchoolServer/index.js
+++ summerCampSchoolServer/index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 require('dotenv').config();
 const jwt = require('jsonwebtoken');
 // This is your test secret API key.
-const stripe = require("stripe")(import.meta.env.STRIPE_SECRET_KEY);
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 const app = express();
 const port = process.env.PORT || 5000;
